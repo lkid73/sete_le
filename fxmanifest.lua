299b5f7cca9d3cbf1d7b6ef0fdffec87b17c4db0
L'kid <tjgenest73@gmail.com>
2024-05-21 08:36:41 -0400

    First Release

---
+++ fxmanifest.lua
@@ -0,0 +1,20 @@
+fx_version "cerulean"
+game "gta5"
+
+author "L'kid"
+description "Skysder's Enhanced Traffic Experience: Lite Edition for FiveM"
+version "4.0"
+
+this_is_a_map "yes"
+
+data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "popgroups.ymt"
+data_file "POPSCHED_FILE" "popcycle.dat"
+data_file "LOADOUTS_FILE" "loadouts.meta"
+data_file "AMBIENT_VEHICLE_MODEL_SET_FILE" "vehiclemodelsets.meta"
+
+files {
+	"popgroups.ymt",
+	"popcycle.dat",
+	"loadouts.meta",
+	"vehiclemodelsets.meta",
+}
