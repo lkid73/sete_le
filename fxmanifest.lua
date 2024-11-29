41374126b37729954cf35bf10a4d70969fe8e042
L'kid <tjgenest73@gmail.com>
2024-11-29 09:58:40 +0100

    Update to 5.2

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -3,18 +3,18 @@ game "gta5"
 
 author "L'kid"
 description "Skysder's Enhanced Traffic Experience: Lite Edition for FiveM"
-version "4.0"
+version "5.0"
+
+dependencies {
+    "/gameBuild:3258",
+}
 
 this_is_a_map "yes"
 
 files {
-	"vehiclemodelsets.meta",
 	"popgroups.ymt",
-	"loadouts.meta",
 	"popcycle.dat",
 }
 
-data_file "AMBIENT_VEHICLE_MODEL_SET_FILE" "vehiclemodelsets.meta"
 data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "popgroups.ymt"
-data_file "LOADOUTS_FILE" "loadouts.meta"
 data_file "POPSCHED_FILE" "popcycle.dat"
