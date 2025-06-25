8fe3092968545cbf205d16a511040163435068c3
L'kid <tjgenest73@gmail.com>
2025-06-25 16:31:50 +0200

    Refacto resource

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -1,20 +1,26 @@
 fx_version "cerulean"
 game "gta5"
+this_is_a_map "yes"
 
 author "L'kid"
 description "Skysder's Enhanced Traffic Experience: Lite Edition for FiveM"
-version "5.0"
-
-dependencies {
-    "/gameBuild:3258",
-}
-
-this_is_a_map "yes"
+version "5.2.2"
 
 files {
-	"popgroups.ymt",
-	"popcycle.dat",
+	"*.ymt",
+	"*.dat",
+	"*.meta",
 }
+
+data_file "SCENARIO_POINTS_OVERRIDE_PSO_FILE" "sp_manifest.ymt"
 
 data_file "FIVEM_LOVES_YOU_341B23A2F0E0F131" "popgroups.ymt"
 data_file "POPSCHED_FILE" "popcycle.dat"
+
+data_file "DISPATCH_DATA_FILE" "data/**/dispatch.meta"
+data_file "LOADOUTS_FILE" "data/**/loadouts.meta"
+data_file "AMBIENT_VEHICLE_MODEL_SET_FILE" "data/**/vehiclemodelsets.meta"
+
+dependencies {
+	"/gameBuild:3258",
+}
